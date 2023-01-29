6ee02b013c94d64c04e882dba716192b220a7d23
FabianAronsson <fabianaronsson2@gmail.com>
2023-01-29 18:37:03 +0100

    Finished main page

--- client/scripts/site.js
+++ client/scripts/site.js
@@ -1,1 +1,14 @@
 let site = "https://api.pokemontcg.io/v2/cards/";
+
+let mainanim = anime({
+    targets: '.landing-page-logo path ',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    fill: '#F5D3C8',
+    easing: 'easeInOutSine',
+    duration: 1000,
+    delay: function(el, i) {
+        return i * 250
+    },
+    direction: 'alternate',
+    loop: false
+});
