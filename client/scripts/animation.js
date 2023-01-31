9dd8cf33200998f354375ce527c641bee1ed1b5a
FabianAronsson <fabianaronsson2@gmail.com>
2023-01-31 15:39:09 +0100

    Added experimental javascript

---
+++ client/scripts/animation.js
@@ -0,0 +1,12 @@
+let mainanim = anime({
+    targets: '.landing-page-logo path ',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    fill: '#F5D3C8',
+    easing: 'easeInOutSine',
+    duration: 1000,
+    delay: function(el, i) {
+        return i * 250
+    },
+    direction: 'alternate',
+    loop: false
+});
--- client/scripts/site.js
+++ client/scripts/site.js
@@ -1,14 +1,79 @@
 let site = "https://api.pokemontcg.io/v2/cards/";
 
-let mainanim = anime({
-    targets: '.landing-page-logo path ',
-    strokeDashoffset: [anime.setDashoffset, 0],
-    fill: '#F5D3C8',
-    easing: 'easeInOutSine',
-    duration: 1000,
-    delay: function(el, i) {
-        return i * 250
-    },
-    direction: 'alternate',
-    loop: false
-});
+
+class Card {
+    constructor(title, description, price, image) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+}
+
+window.onload = async() => {
+    const temp = await fetch('https://api.pokemontcg.io/v2/cards/xy1-1', {
+        method: "GET",
+        'Content-Type': 'application/json',
+        headers: {
+
+        }
+    });
+    const b = await temp.json();
+    console.log(b);
+
+}
+
+let arr = ['test1', 'test2', 'test3']
+
+let products = document.querySelector('#products')
+for (const a of arr) {
+    const markup = `<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="${a}">Open modal for ${a}</button>`;
+    products.innerHTML += markup;
+
+}
+
+const ab = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">New message</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <form>
+        <div class="mb-3">
+          <label for="recipient-name" class="col-form-label">Recipient:</label>
+          <input type="text" class="form-control" id="recipient-name">
+        </div>
+        <div class="mb-3">
+          <label for="message-text" class="col-form-label">Message:</label>
+          <textarea class="form-control" id="message-text"></textarea>
+        </div>
+      </form>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Send message</button>
+    </div>
+  </div>
+</div>
+</div>`;
+
+
+document.body.innerHTML += ab
+
+
+const exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+})
